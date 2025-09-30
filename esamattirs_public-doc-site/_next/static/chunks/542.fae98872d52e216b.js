af266a5d46f8fece48faf4d99f6c31aea3bbb7e3
Esa-Matti Suuronen <esamatti@radiatorsoftware.com>
2025-09-30 11:11:58 +0300

    cleanup

--- esamattirs_public-doc-site/_next/static/chunks/542.fae98872d52e216b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[542],{542:(e,s,k)=>{k.r(s)}}]);
--- esamattirs_public-doc-site/_next/static/tSo5zLZcfycgxqUfoiP5d/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdocs\u002Factions\u002F[slug]","\u002Fdocs\u002Farticles\u002F[slug]","\u002Fdocs\u002Fconfiguration\u002F[slug]","\u002Frelease-notes\u002F[version]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
